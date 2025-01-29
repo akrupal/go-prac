af8ece11962bff75bd0ee10ddadee524027bace0
akrupal <162036218+akrupal@users.noreply.github.com>
2025-01-29 13:12:04 +0000

    commenting main function

--- interview_questions/5.go
+++ interview_questions/5.go
@@ -26,10 +26,10 @@ func write(num int) {
 	}
 	fmt.Println(arr)
 }
-func main() {
-	// 563426789 // 56 crores 34 lakhs 26 thousands 7 hundred and 89
-	// 21563426789 // 2 thousand 1 hundred and 56 crores 34 lakhs 26 thousands 7 hundred and 89
-	// 56 crores 34 lakhs 26 thousands 789
-	// number needs to be printed in words according to the denominations given
-	write(563426789)
-}
+// func main() {
+// 	// 563426789 // 56 crores 34 lakhs 26 thousands 7 hundred and 89
+// 	// 21563426789 // 2 thousand 1 hundred and 56 crores 34 lakhs 26 thousands 7 hundred and 89
+// 	// 56 crores 34 lakhs 26 thousands 789
+// 	// number needs to be printed in words according to the denominations given
+// 	write(563426789)
+// }
